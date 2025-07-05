f7f62c65536bb690da1be1da851660c2d6a010ad
Daniel Fainberg <mail@daniil.fajnberg.de>
2025-07-05 14:22:11 +0200

    feat: log more info when importing contacts

--- src/callbot/contacts.py
+++ src/callbot/contacts.py
@@ -11,19 +11,23 @@ from callbot.schemas.contact import Contact, ContactDB
 
 async def import_contacts(path: Path) -> None:
     await DBEngine().create_tables()
+    count_total, count_imported, count_invalid = 0, 0, 0
     async with DBEngine().get_session() as session:
         with path.open("r") as csvfile:
             reader = DictReader(csvfile)
             for idx, row in enumerate(reader):
+                count_total += 1
                 try:
                     contact = Contact.model_validate(row)
                 except ValidationError as validation_error:
                     log.error(f"Validation error in line {idx}: {validation_error.json()}")
+                    count_invalid += 1
                 else:
-                    await import_contact(session, contact)
+                    count_imported += await _import_contact(session, contact)
+    log.info(f"Imported {count_imported}/{count_total} rows ({count_invalid} invalid)")
 
 
-async def import_contact(db_session: Session, contact: Contact) -> None:
+async def _import_contact(db_session: Session, contact: Contact) -> bool:
     condition = or_(
         col(ContactDB.phone) == contact.phone,
         and_(
@@ -37,5 +41,7 @@ async def import_contact(db_session: Session, contact: Contact) -> None:
         log.info(f"Importing: {contact_json}")
         db_session.add(contact.to_db())
         await db_session.commit()
+        return True
     else:
         log.debug(f"Skipping existing phone/email: {contact_json}")
+        return False
