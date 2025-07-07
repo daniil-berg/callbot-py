67c965d4508e87e9e4a4d6b3045aa6e9042ec729
Daniel Fainberg <mail@daniil.fajnberg.de>
2025-07-07 20:54:02 +0200

    fix(hooks): explicitly export relevant classes at the `hook` package level

--- src/callbot/hooks/__init__.py
+++ src/callbot/hooks/__init__.py
@@ -2,3 +2,12 @@ from .after_call_end import AfterCallEndHook
 from .after_call_start import AfterCallStartHook
 from .before_startup import BeforeStartupHook
 from .hook import Callback, Hook
+
+
+__all__ = [
+    "AfterCallEndHook",
+    "AfterCallStartHook",
+    "BeforeStartupHook",
+    "Callback",
+    "Hook",
+]
