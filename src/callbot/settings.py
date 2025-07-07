65e8ff313b881edcbeace75f2c16f56319e4a94f
Daniel Fainberg <mail@daniil.fajnberg.de>
2025-07-07 20:10:46 +0200

    feat(settings)!: allow plugins to define their own config sections
    The `Settings` model now has a `plugins` field of the `PluginsSettings` type, which accepts extra data.
    The new `Settings.plugin` method provides a way to access custom plugin settings in a Pydantic type safe way at runtime.
    The `model_dump_yaml` method is now available to all `SettingsSection` classes.
    `OpenAISessionOptions` is now called `SessionSettings`.
    `MiscellaneousSettings` is now called `MiscSettings`.
    The source for the PyYAML workaround is no longer missing.

--- src/callbot/settings.py
+++ src/callbot/settings.py
@@ -1,5 +1,6 @@
 from collections.abc import Callable
 from datetime import time
+from functools import cache
 from ipaddress import IPv4Address
 from logging import CRITICAL, INFO, getLevelNamesMapping
 from pathlib import Path
@@ -7,6 +8,7 @@ from typing import Annotated, Any, ClassVar, Literal, TypeAlias
 
 import yaml
 from annotated_types import Ge, Le
+from caseutil import to_snake
 from pydantic import (
     BaseModel,
     BeforeValidator,
@@ -148,173 +150,10 @@ class SettingsSection(BaseModel):
                         raise e from None
             raise e
 
-
-class AuthSettings(SettingsSection):
-    secret: str = "secret"
-    alg: Algorithm = DEFAULT_ALGORITHM
-    iss: str | None = None
-    aud: str | None = None
-    expiration_seconds: int = 15 * 60
-
-
-class ServerSettings(SettingsSection):
-    host: IPvAnyAddress = IPv4Address("127.0.0.1")
-    port: PositiveInt = 8000
-    public_base_url: HttpUrl | None = None
-    auth: AuthSettings = AuthSettings()
-
-
-class DBSettings(SettingsSection):
-    driver: str = "sqlite+aiosqlite"
-    username: str | None = None
-    password: str | None = None
-    host: str | None = None
-    port: int | None = None
-    name: str | None = None
-    query: DBURLQuery = {}
-
-    @property
-    def url(self) -> URL:
-        return URL.create(
-            drivername=self.driver,
-            username=self.username,
-            password=self.password,
-            host=self.host,
-            port=self.port,
-            database=self.name,
-            query=self.query,
-        )
-
-
-class TwilioSettings(SettingsSection):
-    account_sid: StrNoneAsEmpty = ""
-    auth_token: SecretStrNoneAsEmpty = SecretStr("")
-    phone_number: StrPhone | None = None
-    max_parallel_calls: PositiveInt = 1
-
-
-class OpenAISessionOptions(SettingsSection):
-    instructions: Annotated[
-        PathFileExists | Str128 | None,
-        Field(union_mode="left_to_right"),
-        WrapSerializer(workaround_shorten_128),
-    ] = None
-    model: Literal[
-        "gpt-4o-mini-realtime-preview",
-        "gpt-4o-mini-realtime-preview-2024-12-17",
-        "gpt-4o-realtime-preview",
-        "gpt-4o-realtime-preview-2024-10-01",
-        "gpt-4o-realtime-preview-2024-12-17",
-        "gpt-4o-realtime-preview-2025-06-03",
-    ] = "gpt-4o-realtime-preview-2025-06-03"
-    speed: FloatOpenAISpeed = 1.0
-    temperature: FloatOpenAITemperature = 0.8
-    voice: OpenAIVoice = "sage"
-
-
-class OpenAISettings(SettingsSection):
-    REALTIME_BASE_URL: ClassVar[str] = "wss://api.openai.com/v1/realtime"
-    AUDIO_FORMAT: ClassVar[
-        Literal["pcm16", "g711_ulaw", "g711_alaw"]
-    ] = "g711_ulaw"
-
-    api_key: SecretStrNoneAsEmpty = SecretStr("")
-    init_conversation_prompt: Annotated[
-        PathFileExists | Str128 | None,
-        Field(union_mode="left_to_right"),
-        WrapSerializer(workaround_shorten_128),
-    ] = None
-    log_event_types: list[str] = [
-        "input_audio_buffer.committed",
-        "input_audio_buffer.speech_started",
-        "input_audio_buffer.speech_stopped",
-        "rate_limits.updated",
-        "response.content.done",
-        "response.done",
-        "session.created",
-        "session.updated",
-    ]
-    session: OpenAISessionOptions = OpenAISessionOptions()
-
-    @property
-    def realtime_stream_url(self) -> str:
-        return f"{self.REALTIME_BASE_URL}?model={self.session.model}"
-
-    def get_realtime_auth_headers(self) -> StrDict:
-        if not self.api_key:
-            raise RuntimeError("No OpenAI API key configured")
-        return {
-            "Authorization": f"Bearer {self.api_key.get_secret_value()}",
-            "OpenAI-Beta": "realtime=v1",
-        }
-
-    def get_init_conversation_prompt(self) -> str | None:
-        return get_text_from_file_or_str(self.init_conversation_prompt)
-
-    def get_session_instructions(self) -> str | None:
-        return get_text_from_file_or_str(self.session.instructions)
-
-
-class ScheduleSettings(SettingsSection):
-    business_start: time = time(hour=8)
-    business_end: time = time(hour=18)
-    business_days: frozenset[Literal[0, 1, 2, 3, 4, 5, 6]] = frozenset({0, 1, 2, 3, 4})
-    retry_delay_hours: PositiveInt = 4
-
-
-class LoggingSettings(SettingsSection):
-    level: IntLogLevel = INFO
-    format: str = "<level>{level: <8}</level> | <level>{message}</level> | <cyan>{name}</cyan>"
-    modules: LogModules = {
-        "aiosqlite": False,
-        "sqlalchemy": False,
-        "websockets": False,
-    }
-
-
-class MiscellaneousSettings(SettingsSection):
-    default_phone_region: str | None = None
-    mode: Literal["testing", "production"] = "testing"
-
-
-class Settings(
-    BaseSettings,
-    SettingsSection,
-    metaclass=Singleton.from_meta(ModelMetaclass),  # type: ignore[misc]
-):
-    """
-    Encapsulates all application settings and instantiates a singleton.
-    """
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_nested_delimiter="__",
-        yaml_file="config.yaml",
-    )
-
-    server: ServerSettings = ServerSettings()
-    db: DBSettings = DBSettings()
-    twilio: TwilioSettings = TwilioSettings()
-    openai: OpenAISettings = OpenAISettings()
-    schedule: ScheduleSettings = ScheduleSettings()
-    logging: LoggingSettings = LoggingSettings()
-    misc: MiscellaneousSettings = MiscellaneousSettings()
-
     @classmethod
-    def settings_customise_sources(
-        cls,
-        settings_cls: type[BaseSettings],
-        init_settings: PydanticBaseSettingsSource,
-        env_settings: PydanticBaseSettingsSource,
-        dotenv_settings: PydanticBaseSettingsSource,
-        file_secret_settings: PydanticBaseSettingsSource,
-    ) -> tuple[PydanticBaseSettingsSource, ...]:
-        return (
-            init_settings,
-            env_settings,
-            dotenv_settings,
-            file_secret_settings,
-            YamlConfigSettingsSource(settings_cls),
-        )
+    @cache
+    def section_name(cls) -> str:
+        return to_snake(cls.__name__.rsplit("Settings", maxsplit=1)[0])
 
     def model_dump_yaml(
         self,
@@ -355,7 +194,201 @@ class Settings(
         )
 
 
+class AuthSettings(SettingsSection):
+    secret: str = "secret"
+    alg: Algorithm = DEFAULT_ALGORITHM
+    iss: str | None = None
+    aud: str | None = None
+    expiration_seconds: int = 15 * 60
+
+
+class ServerSettings(SettingsSection):
+    host: IPvAnyAddress = IPv4Address("127.0.0.1")
+    port: PositiveInt = 8000
+    public_base_url: HttpUrl | None = None
+    auth: AuthSettings = AuthSettings()
+
+
+# TODO: Narrow field types.
+class DBSettings(SettingsSection):
+    driver: str = "sqlite+aiosqlite"
+    username: str | None = None
+    password: str | None = None
+    host: str | None = None
+    port: int | None = None
+    name: str | None = None
+    query: DBURLQuery = {}
+
+    @property
+    def url(self) -> URL:
+        return URL.create(
+            drivername=self.driver,
+            username=self.username,
+            password=self.password,
+            host=self.host,
+            port=self.port,
+            database=self.name,
+            query=self.query,
+        )
+
+
+class TwilioSettings(SettingsSection):
+    account_sid: StrNoneAsEmpty = ""
+    auth_token: SecretStrNoneAsEmpty = SecretStr("")
+    phone_number: StrPhone | None = None
+    max_parallel_calls: PositiveInt = 1
+
+
+class SessionSettings(SettingsSection):
+    instructions: Annotated[
+        PathFileExists | Str128 | None,
+        Field(union_mode="left_to_right"),
+        WrapSerializer(workaround_shorten_128),
+    ] = None
+    model: Literal[
+        "gpt-4o-mini-realtime-preview",
+        "gpt-4o-mini-realtime-preview-2024-12-17",
+        "gpt-4o-realtime-preview",
+        "gpt-4o-realtime-preview-2024-10-01",
+        "gpt-4o-realtime-preview-2024-12-17",
+        "gpt-4o-realtime-preview-2025-06-03",
+    ] = "gpt-4o-realtime-preview-2025-06-03"
+    speed: FloatOpenAISpeed = 1.0
+    temperature: FloatOpenAITemperature = 0.8
+    voice: OpenAIVoice = "sage"
+
+
+class OpenAISettings(SettingsSection):
+    REALTIME_BASE_URL: ClassVar[str] = "wss://api.openai.com/v1/realtime"
+    AUDIO_FORMAT: ClassVar[
+        Literal["pcm16", "g711_ulaw", "g711_alaw"]
+    ] = "g711_ulaw"
+
+    api_key: SecretStrNoneAsEmpty = SecretStr("")
+    init_conversation_prompt: Annotated[
+        PathFileExists | Str128 | None,
+        Field(union_mode="left_to_right"),
+        WrapSerializer(workaround_shorten_128),
+    ] = None
+    log_event_types: list[str] = [
+        "input_audio_buffer.committed",
+        "input_audio_buffer.speech_started",
+        "input_audio_buffer.speech_stopped",
+        "rate_limits.updated",
+        "response.content.done",
+        "response.done",
+        "session.created",
+        "session.updated",
+    ]
+    session: SessionSettings = SessionSettings()
+
+    @property
+    def realtime_stream_url(self) -> str:
+        return f"{self.REALTIME_BASE_URL}?model={self.session.model}"
+
+    def get_realtime_auth_headers(self) -> StrDict:
+        if not self.api_key:
+            raise RuntimeError("No OpenAI API key configured")
+        return {
+            "Authorization": f"Bearer {self.api_key.get_secret_value()}",
+            "OpenAI-Beta": "realtime=v1",
+        }
+
+    def get_init_conversation_prompt(self) -> str | None:
+        return get_text_from_file_or_str(self.init_conversation_prompt)
+
+    def get_session_instructions(self) -> str | None:
+        return get_text_from_file_or_str(self.session.instructions)
+
+    @classmethod
+    def section_name(cls) -> str:
+        return "openai"
+
+
+class ScheduleSettings(SettingsSection):
+    business_start: time = time(hour=8)
+    business_end: time = time(hour=18)
+    business_days: frozenset[Literal[0, 1, 2, 3, 4, 5, 6]] = frozenset({0, 1, 2, 3, 4})
+    retry_delay_hours: PositiveInt = 4
+
+
+class LoggingSettings(SettingsSection):
+    level: IntLogLevel = INFO
+    format: str = "<level>{level: <8}</level> | <level>{message}</level> | <cyan>{name}</cyan>"
+    modules: LogModules = {
+        "aiosqlite": False,
+        "sqlalchemy": False,
+        "websockets": False,
+    }
+
+
+class MiscSettings(SettingsSection):
+    default_phone_region: str | None = None
+    mode: Literal["testing", "production"] = "testing"
+
+
+class PluginsSettings(SettingsSection):
+    model_config = ConfigDict(extra="allow")
+
+
+class Settings(
+    BaseSettings,
+    SettingsSection,
+    metaclass=Singleton.from_meta(ModelMetaclass),  # type: ignore[misc]
+):
+    """
+    Encapsulates all application settings and instantiates a singleton.
+    """
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_nested_delimiter="__",
+        yaml_file="config.yaml",
+    )
+
+    server: ServerSettings = ServerSettings()
+    db: DBSettings = DBSettings()
+    twilio: TwilioSettings = TwilioSettings()
+    openai: OpenAISettings = OpenAISettings()
+    schedule: ScheduleSettings = ScheduleSettings()
+    logging: LoggingSettings = LoggingSettings()
+    misc: MiscSettings = MiscSettings()
+    plugins: PluginsSettings = PluginsSettings()
+
+    _plugin_cache: dict[str, SettingsSection] = {}
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return (
+            init_settings,
+            env_settings,
+            dotenv_settings,
+            file_secret_settings,
+            YamlConfigSettingsSource(settings_cls),
+        )
+
+    def plugin[S: SettingsSection](self, section_class: type[S]) -> S:
+        if section_class.__qualname__ in self._plugin_cache:
+            return self._plugin_cache[section_class.__qualname__]
+        name = section_class.section_name()
+        data = self.plugins.__pydantic_extra__.get(name)
+        if data is None:
+            # Assume the section model has defaults for all fields.
+            section = section_class()
+        else:
+            section = section_class.model_validate(data)
+        self._plugin_cache[section_class.__qualname__] = section
+        return section
+
+
 # The following is needed because PyYAML is annoying.
+# Source: https://stackoverflow.com/a/45004775/
 
 yaml.SafeDumper.org_represent_str = yaml.SafeDumper.represent_str  # type: ignore[attr-defined]
 
